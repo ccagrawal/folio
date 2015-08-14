54d20981e95e133d888717bc470e57503c6a3090
ccagrawal <ccagrawal@gmail.com>
2015-08-14 13:45:43 -0500

    Make example dashboard

--- server.R
+++ server.R
@@ -1,4 +1,25 @@
 library(shiny)
 
-shinyServer(function(input, output) { 
+shinyServer(function(input, output) {
+  
+  set.seed(122)
+  histdata <- rnorm(500)
+  
+#   output$messageMenu <- renderMenu({
+#     # Code to generate each of the messageItems here, in a list. This assumes
+#     # that messageData is a data frame with two columns, 'from' and 'message'.
+#     msgs <- apply(messageData, 1, function(row) {
+#       messageItem(from = row[["from"]], message = row[["message"]])
+#     })
+#     
+#     # This is equivalent to calling:
+#     #   dropdownMenu(type="messages", msgs[[1]], msgs[[2]], ...)
+#     dropdownMenu(type = "messages", .list = msgs)
+#   })
+  
+  output$plot1 <- renderPlot({
+    data <- histdata[seq_len(input$slider)]
+    hist(data)
+  })
+  
 })
--- ui.R
+++ ui.R
@@ -1,7 +1,87 @@
 library(shinydashboard)
 
-dashboardPage(
-  dashboardHeader(),
-  dashboardSidebar(),
-  dashboardBody()
+messageMenu <- dropdownMenu(type = "messages",
+  messageItem(
+    from = "Sales Dept",
+    message = "Sales are steady this month."
+  ),
+  messageItem(
+    from = "New User",
+    message = "How do I register?",
+    icon = icon("question"),
+    time = "13:45"
+  ),
+  messageItem(
+    from = "Support",
+    message = "The new server is ready.",
+    icon = icon("life-ring"),
+    time = "2014-12-01"
+  )
 )
+
+notificationMenu <- dropdownMenu(type = "notifications",
+  notificationItem(
+    text = "5 new users today",
+    icon("users")
+  ),
+  notificationItem(
+    text = "12 items delivered",
+    icon("truck"),
+    status = "success"
+  ),
+  notificationItem(
+    text = "Server load at 86%",
+    icon = icon("exclamation-triangle"),
+    status = "warning"
+  )
+)
+
+taskMenu <- dropdownMenu(type = "tasks", badgeStatus = "success",
+  taskItem(value = 90, color = "green",
+    "Documentation"
+  ),
+  taskItem(value = 17, color = "aqua",
+    "Project X"
+  ),
+  taskItem(value = 75, color = "yellow",
+    "Server deployment"
+  ),
+  taskItem(value = 80, color = "red",
+    "Overall project"
+  )
+)
+
+header <- dashboardHeader(title = "Folio",
+                          messageMenu, notificationMenu, taskMenu)
+
+sidebar <- dashboardSidebar(
+  sidebarMenu(
+    menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+    menuItem("Widgets", tabName = "widgets", icon = icon("th"))
+  )
+)
+
+body <- dashboardBody(
+  tabItems(
+    # First tab content
+    tabItem(tabName = "dashboard",
+            fluidRow(
+              box(title = "Histogram", status = "primary", plotOutput("plot2", height = 250)),
+              
+              box(
+                title = "Inputs", status = "warning",
+                "Box content here", br(), "More box content",
+                sliderInput("slider", "Slider input:", 1, 100, 50),
+                textInput("text", "Text input:")
+              )
+            )
+    ),
+    
+    # Second tab content
+    tabItem(tabName = "widgets",
+            h2("Widgets tab content")
+    )
+  )
+)
+
+dashboardPage(header, sidebar, body)
